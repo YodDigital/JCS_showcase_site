e0706cdc41903121ac49ac132b7702b7a43f065f
YodDigital <yoddigital@gmail.com>
2024-02-24 12:53:33 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'JCS_showcase_site';
+}
